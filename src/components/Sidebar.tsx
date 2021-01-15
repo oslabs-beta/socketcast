85b6d3ee256bffc2d850d8f66a4ac5ae544f19d1
Will <whbladon@gmail.com>
2021-01-15 11:01:54 -0800

    updating keys and mapping

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,36 +1,31 @@
 import React from "react";
 
 function Sidebar() {
-  let servers = [
-    { name: "Server1" },
-    { name: "Server2" },
+  let onlineServers = [{ name: "Server1" }, { name: "Server2" }];
+  let offlineServers = [
     { name: "Server3" },
     { name: "Server4" },
     { name: "Server5" },
   ];
+
   return (
     <div className="sidebar">
-
-
       <b>Running Servers</b>
       <div className="sidebar_container">
-        {servers.map((server) => (
-          <div className="sidebar_server" id = 'server.name'>
+        {onlineServers.map((server) => (
+          <div className="sidebar_server" key={server.name}>
             <b>{server.name}</b>
           </div>
         ))}
       </div>
 
-
       <b>Stopped Servers</b>
       <div className="sidebar_container">
-        {servers.map((server) => {
-          return (
-            <div className="sidebar_server" id = 'server.name'>
-              <b>{server.name}</b>
-            </div>
-          );
-        })}
+        {offlineServers.map((server) => (
+          <div className="sidebar_server" key={server.name}>
+            <b>{server.name}</b>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/StreamDisplay.tsx
+++ src/components/StreamDisplay.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 function StreamDisplay() {
+  let inputStream = [
+    { message: "something something", id: 1 },
+    { message: "something something", id: 2 },
+    { message: "something something", id: 3 },
+  ];
+  let outputStream = [
+    { message: "something something", id: 1 },
+    { message: "something something", id: 2 },
+    { message: "something something", id: 3 },
+  ];
+
   return (
     <div className="streamDisplay">
       <div className="streamDisplay_header">Streamdisplay event chat</div>
@@ -8,25 +19,28 @@ function StreamDisplay() {
         <div>Stream Input</div>
         <br />
         <div className="streamDisplay_outputbox">
-          <div className="code">message: 'something something something'</div>
-          <div className="code">message: 'something something something'</div>
-          <div className="code">message: 'something something something'</div>
-          <div className="code">message: 'something something something'</div>
+          {inputStream.map((code) => (
+            <div className="code" key={code.id}>
+              message: 'something something something'
+            </div>
+          ))}
         </div>
         <div>
-          <button className = 'streamDisplay_button'>improvised response</button>
-          <button className = 'streamDisplay_button'>application/json</button>
+          <button className="streamDisplay_button">improvised response</button>
+          <button className="streamDisplay_button">application/json</button>
         </div>
       </div>
       <div className="streamDisplay_container">
         <div>Stream Output</div>
         <br />
         <div className="streamDisplay_inputbox">
-          <div className="code">message: 'something something something'</div>
-          <div className="code">message: 'something something something'</div>
-          <div className="code">message: 'something something something'</div>
+          {outputStream.map((code) => (
+            <div className="code" key={code.id}>
+              message: 'something something something'
+            </div>
+          ))}
         </div>
-        <button className = 'streamDisplay_button'>emit response</button>
+        <button className="streamDisplay_button">emit response</button>
       </div>
     </div>
   );
