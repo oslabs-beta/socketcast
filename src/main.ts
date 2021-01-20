4989ae7d670c927423e9fa1897c2ae0dcc9a4c2a
Christopher Docuyanan <christophejd@gmail.com>
2021-01-19 22:42:29 -0800

    Added code ready for electron-devtools-installer. See comments in code for details on why it might not work.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from 'electron';
+import installExtension, { REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
 // make mainWindow var (specify types: alive or dead)
 let mainWindow: Electron.BrowserWindow | null;
@@ -24,6 +25,23 @@ const createWindow = (): void => {
   });
 };
 
+const onReady = (): void => {
+  createWindow();
+};
+
+/**
+ * There is a potential issue with Electron version 9+ when trying to use Redux Dev Tools
+ * https://github.com/electron/electron/issues/24011
+ * https://github.com/electron/electron/issues/24638
+ */
+// app.whenReady().then(() => {
+//   installExtension(REDUX_DEVTOOLS)
+//     .then((name) => console.log(`Added Extension:  ${name}`))
+//     .catch((err) => console.log('An error occurred: ', err));
+//   installExtension(REACT_DEVELOPER_TOOLS)
+//     .then((name) => console.log(`Added Extension:  ${name}`))
+//     .catch((err) => console.log('An error occurred: ', err));
+// });
+
 // when app is 'ready' instantiate mainWindow with our function
-app.on('ready', createWindow);
-// app.on('ready', createTestWindow)
+app.on('ready', onReady);
