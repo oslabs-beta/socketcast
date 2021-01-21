d1c4dfc123cbc3034764bf002b1ce3ee071d3492
Will <whbladon@gmail.com>
2021-01-20 21:23:54 -0800

    css cleanup

--- src/components/StreamDisplay.tsx
+++ src/components/StreamDisplay.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-import StreamInput from "./StreamInput";
+import React from 'react';
+import StreamInput from './StreamInput';
 // may use for conditional rendering in future
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
 function StreamDisplay() {
+
+
   //we should figure out how to fix this work-around
   // @ts-ignore
-  const currentServerId = useSelector(
-    (store) => store.serversReducer.currentServerId
-  );
+  const currentServerId = useSelector(store => store.serversReducer.currentServerId)
   // @ts-ignore
-  const outputStream = useSelector((store) => store.dataReducer.streams);
+  const outputStream = useSelector(store => store.dataReducer.streams);
+  
+  let counter = 0
 
-  let counter = 0;
 
   return (
     <div className="streamDisplay">
@@ -21,12 +22,14 @@ function StreamDisplay() {
         <div>Stream Output</div>
         <br />
         <div className="streamDisplay_outputbox">
-          {currentServerId &&
-            outputStream[currentServerId].map((code: string) => (
-              <div className="code" key={counter++}>
-                {code}
-              </div>
-            ))}
+
+          {currentServerId && outputStream[currentServerId].map((code: string) => (
+            <div className="code" key={counter++}>
+              {code}
+            </div>
+
+
+          ))}
         </div>
       </div>
     </div>
