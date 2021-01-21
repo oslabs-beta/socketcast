09a80cbb542e691eccf097c5ff48eb5e5fc739d3
Will <whbladon@gmail.com>
2021-01-20 21:10:07 -0800

    cleaned up StreamDisplay

--- src/components/StreamDisplay.tsx
+++ src/components/StreamDisplay.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import StreamInput from './StreamInput';
+import React from "react";
+import StreamInput from "./StreamInput";
 // may use for conditional rendering in future
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 function StreamDisplay() {
-
-
   //we should figure out how to fix this work-around
   // @ts-ignore
-  const currentServerId = useSelector(store => store.serversReducer.currentServerId)
+  const currentServerId = useSelector(
+    (store) => store.serversReducer.currentServerId
+  );
   // @ts-ignore
-  const outputStream = useSelector(store => store.dataReducer.streams);
-  
-  let counter = 0
+  const outputStream = useSelector((store) => store.dataReducer.streams);
 
+  let counter = 0;
 
   return (
     <div className="streamDisplay">
@@ -22,17 +21,12 @@ function StreamDisplay() {
         <div>Stream Output</div>
         <br />
         <div className="streamDisplay_outputbox">
-
-          <button onClick = {()=>{console.log(outputStream)}} >console log data stream</button>
-          <button onClick = {()=>{console.log(currentServerId)}}>console log current server id</button>
-
-          {currentServerId && outputStream[currentServerId].map((code: string) => (
-            <div className="code" key={counter++}>
-              {code}
-            </div>
-
-
-          ))}
+          {currentServerId &&
+            outputStream[currentServerId].map((code: string) => (
+              <div className="code" key={counter++}>
+                {code}
+              </div>
+            ))}
         </div>
       </div>
     </div>
