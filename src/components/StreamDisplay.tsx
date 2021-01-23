cddaa67420e37be8c32703a3c43d205b95e15203
Christopher Docuyanan <christophejd@gmail.com>
2021-01-23 10:05:38 -0800

    Added new generic CSS classes. Cleaned up Redux TypeScript type for useSelector.

--- src/components/StreamDisplay.tsx
+++ src/components/StreamDisplay.tsx
@@ -3,32 +3,27 @@
  */
 
 import React from 'react';
-import StreamInput from './StreamInput';
 import { useSelector } from 'react-redux';
+import { RootState } from '@/store/reducers';
+import StreamInput from './StreamInput';
 
 function StreamDisplay() {
+  const currentServerId = useSelector((store: RootState) => store.serversReducer.currentServerId);
+  const outputStream = useSelector((store: RootState) => store.dataReducer.streams);
 
-
-  //we should figure out how to fix this work-around
-  // @ts-ignore
-  const currentServerId = useSelector(store => store.serversReducer.currentServerId)
-  // @ts-ignore
-  const outputStream = useSelector(store => store.dataReducer.streams);
-
-  let counter = 0
-
+  let counter = 0;
 
   return (
     <div className="stream-column">
       <div className="streamDisplay_container streamDisplay_outputContainer">
-        <div className="window_title" >CONSOLE</div>
+        <div className="header-container">
+          <h3 className="header-primary">CONSOLE</h3>
+        </div>
         <div className="streamDisplay_outputbox">
-
           {currentServerId && outputStream[currentServerId].map((code: string) => (
             <div className="code" key={counter++}>
               {code}
             </div>
-
 
           ))}
         </div>
