ebae9980134888f37bc31a8ecbd1d3acca8c5579
Will <whbladon@gmail.com>
2021-02-02 17:19:07 -0800

    scrollbar automove

--- src/components/StreamDisplay.tsx
+++ src/components/StreamDisplay.tsx
@@ -2,13 +2,21 @@
  * @description Right pane of application. Displays input and output streams
  */
 
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/reducers';
 import Highlight from 'react-highlight'
 
+// const AlwaysScrollToBottom = () => {
+//   const elementRef: any = useRef();
+//   useEffect(() => elementRef.current.scrollIntoView());
+//   return <div ref={elementRef} />;
+// };
+
 
 function StreamDisplay() {
+
+
   const currentServerId = useSelector(
     (store: RootState) => store.navigation.currentServerId,
   );
@@ -25,6 +33,7 @@ function StreamDisplay() {
           && outputStream[currentServerId].map((code: string) => (
             <div className="code" key={counter++}>
               <Highlight className = 'json'>{code.toString()}</Highlight>
+              {/* <AlwaysScrollToBottom/> */}
             </div>
           ))}
       </div>
